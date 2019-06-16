df706738894bd0991e7035f99912a270b3cf444e
clarettetd <48140509+cterrasid@users.noreply.github.com>
2019-06-17 00:24:56 +0200

    Create docs folder

---
+++ docs/precache-manifest.3aaad6531c621848c8cbf9f762c6920f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a815601b8edb553b6ea0b3f8209f3e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dca34bdad29a8697660a",
+    "url": "./static/css/main.38a67045.chunk.css"
+  },
+  {
+    "revision": "246f946294448963bb77",
+    "url": "./static/js/2.10af5e64.chunk.js"
+  },
+  {
+    "revision": "dca34bdad29a8697660a",
+    "url": "./static/js/main.372c546e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
