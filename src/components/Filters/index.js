585ca7cb2149e9a16d8610683abad26340e9772f
cterrasid <clarette.terrasi@gmail.com>
2019-06-16 21:27:22 +0200

    Create styles for Filters component

--- src/components/Filters/index.js
+++ src/components/Filters/index.js
@@ -1,21 +1,25 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import PropTypes from 'prop-types';
+import './styles.scss';
 
 const Filters = props => {
   const { filterByName, queryName } = props;
 
   return (
-    <form>
-      <label htmlFor="filter-name">
-        Find by <strong>name</strong>
-        <input
-          id="filter-name"
-          type="text"
-          placeholder="Put the name of a pokemon"
-          onChange={filterByName}
-          queryName={queryName}
-        />
+    <form className="filter">
+      <label className="filter-name__label" htmlFor="filter-name">
+        Find by name
       </label>
+      <input
+        className="filter-name__input"
+        id="filter-name"
+        type="text"
+        placeholder="Put the name of a pokemon"
+        onChange={filterByName}
+        queryName={queryName}
+      />
     </form>
   );
 };
