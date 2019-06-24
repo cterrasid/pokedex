86ea2d0f07fe1b26f85d0847f7425cac6bdd51c9
cterrasid <clarette.terrasi@gmail.com>
2019-06-24 17:52:13 +0200

    Add more data to card detail

--- src/components/Detail/index.js
+++ src/components/Detail/index.js
@@ -5,21 +5,53 @@ import './styles.scss';
 
 const Detail = props => {
   const { detail } = props;
+  console.log(detail);
 
   return (
     <Fragment>
       {detail ? (
         <div className="detail__container">
-          <div className="detail__image-container">
+          <div className="detail__main-wrapper">
+            <h2>{detail.name}</h2>
+            <h3>#{detail.id}</h3>
             <img
-              className="detail__image"
+              className="detail__image front"
               src={detail.sprites.front_default}
               alt={detail.name}
             />
+            <img
+              className="detail__image back"
+              src={detail.sprites.back_default}
+              alt={detail.name}
+            />
+            <ul className="detail__types">
+              {detail.types.map((type, index) => (
+                <li key={`${detail.id + index}`} className="detail__types-type">
+                  {type.type.name}
+                </li>
+              ))}
+            </ul>
           </div>
-          <p>{detail.name}</p>
-          <p>{detail.height}</p>
-          <p>{detail.weight}</p>
+          <ul className="detail__profile">
+            <h3 className="detail__profile-title">Profile</h3>
+            <li>
+              <strong>Height:</strong> {detail.height / 10} m
+            </li>
+            <li>
+              <strong>Weight:</strong> {detail.weight / 10} Kg
+            </li>
+            <ul>
+              <strong>Abilities:</strong>
+              {detail.abilities.map((ability, index) => (
+                <li
+                  key={`${detail.id + index}`}
+                  className="detail__profile-abilities"
+                >
+                  {ability.ability.name}
+                </li>
+              ))}
+            </ul>
+          </ul>
         </div>
       ) : (
         <p>Error</p>
