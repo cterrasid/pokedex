0139a5f0aadfce8f737e9a378ce612e9d96abe38
cterrasid <clarette.terrasi@gmail.com>
2019-06-24 19:03:45 +0200

    update docs

---
+++ docs/precache-manifest.b47dd6cf7e65a5dea0840696ef344241.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64248a95e85fb47a77a9cbd730b75738",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9e1f50802fe22fd46d69",
+    "url": "./static/css/main.43ab4208.chunk.css"
+  },
+  {
+    "revision": "27df850c9c93923d1bda",
+    "url": "./static/js/2.33ac7452.chunk.js"
+  },
+  {
+    "revision": "9e1f50802fe22fd46d69",
+    "url": "./static/js/main.84b712ed.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
